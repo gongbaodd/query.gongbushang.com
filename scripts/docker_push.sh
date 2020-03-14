4e3fd82cf1e37b635d5e6b721a58f34547e73579
gongbaodd@Inspiron_Ubuntu <gongbaodd@qq.com>
2020-03-14 12:10:38 +0800

    🐛 Fix: Fix docker push script

--- scripts/docker_push.sh
+++ scripts/docker_push.sh
@@ -1,3 +1,5 @@
-docker login -u "$DOCKER_USERNAME" -p $DOCKER_PASSWORD $DOCKER_REGISTRY
-docker tag query_gongbushang_com $DOCKER_REGISTRY/query_gongbushang_com
-docker push $DOCKER_REGISTRY/query_gongbushang_com
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME"  $DOCKER_REGISTRY  --password-stdin
+
+docker build Dockerfile -t query_gongbushang_com:latest
+docker tag query_gongbushang_com:latest $DOCKER_REGISTRY/query_gongbushang_com:latest
+docker push $DOCKER_REGISTRY/query_gongbushang_com:latest
