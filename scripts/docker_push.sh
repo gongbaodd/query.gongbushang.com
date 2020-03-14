0de0bab2b859da81ba99871b5766489ad2256722
gongbaodd@Inspiron_Ubuntu <gongbaodd@qq.com>
2020-03-14 13:24:21 +0800

    🐛 Fix: Fix docker deployment

--- scripts/docker_push.sh
+++ scripts/docker_push.sh
@@ -1,5 +1,5 @@
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME"  $DOCKER_REGISTRY  --password-stdin
 
-docker build Dockerfile -t query_gongbushang_com:latest
+docker build -t query_gongbushang_com:latest .
 docker tag query_gongbushang_com:latest $DOCKER_REGISTRY/query_gongbushang_com:latest
 docker push $DOCKER_REGISTRY/query_gongbushang_com:latest
